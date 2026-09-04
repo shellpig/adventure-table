2fdcc2dfbdb6a26f6f202a8b550abc002174f970
shellpig <shellpig@gmail.com>
2026-09-04 15:04:14 +0800

    feat(m03-b): register export router

--- apps/server/app/api/__init__.py
+++ apps/server/app/api/__init__.py
@@ -1,10 +1,12 @@
 from app.api.character_builder import router as character_builder_router
+from app.api.character_export import router as character_export_router
 from app.api.characters import router as characters_router
 from app.api.content_presentation import router as content_presentation_router
 from app.api.reference import router as reference_router
 
 __all__ = [
     "character_builder_router",
+    "character_export_router",
     "characters_router",
     "content_presentation_router",
     "reference_router",
