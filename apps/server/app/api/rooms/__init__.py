67b4cad31d858806af675d018b35ead71c2f9be1
shellpig <shellpig@gmail.com>
2026-09-10 11:28:18 +0800

    feat(p3d): register AI controller routes

--- apps/server/app/api/rooms/__init__.py
+++ apps/server/app/api/rooms/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.rooms.access import router as access_router
+from app.api.rooms.ai_controllers import router as ai_controllers_router
 from app.api.rooms.campaigns import router as campaigns_router
 from app.api.rooms.character_builder import router as character_builder_router
 from app.api.rooms.characters import router as characters_router
@@ -18,6 +19,7 @@ router.include_router(access_router)
 router.include_router(campaigns_router)
 router.include_router(seats_router)
 router.include_router(sessions_router)
+router.include_router(ai_controllers_router)
 router.include_router(table_events_router)
 router.include_router(exploration_router)
 router.include_router(p3c_rolls_router)
