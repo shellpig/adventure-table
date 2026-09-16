3f2e1f442225b7d10c2aff5d4d8e21f70d732bdf
shellpig <shellpig@gmail.com>
2026-09-16 14:34:14 +0800

    test(p4d): allow concentration state writer

--- apps/server/tests/test_character_state_writer_guard.py
+++ apps/server/tests/test_character_state_writer_guard.py
@@ -9,6 +9,7 @@ ALLOWED_STATE_UPDATE_FILES = {
     "persistence/characters.py",
     "persistence/state_mutations.py",
     "persistence/combat/attacks.py",
+    "persistence/combat/concentration.py",
     "persistence/combat/core_rolls.py",
     "persistence/combat/resolution.py",
     "persistence/combat/special_attacks.py",
